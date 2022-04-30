fe4fddebc8ca95c505cd011ef48b672cf70de8f9
Justin Merrell <merrelljustin@gmail.com>
2022-04-30 09:36:59 -0400

    change view

--- gui_interface/modules/end_screen.py
+++ gui_interface/modules/end_screen.py
@@ -10,5 +10,5 @@ class EndScreen(ttk.Frame):
         ttk.Frame.__init__(self, main_window)
         self.main_window = main_window
 
-        label = ttk.Label(self, text="Page 2")
+        label = ttk.Label(text="Page 2")
         label.grid(row=0, column=4, padx=10, pady=10)
