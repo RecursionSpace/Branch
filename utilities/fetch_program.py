3548488e6d36964833d8ffb76d463f041af37d2c
Justin Merrell <merrelljustin@gmail.com>
2022-05-02 13:34:59 -0400

    Update fetch_program.py

--- utilities/fetch_program.py
+++ utilities/fetch_program.py
@@ -7,7 +7,7 @@ from git import Repo
 
 def clone(url):
     ''' Clones the repository from the given URL. '''
-    remote_url = repo.remotes[0].config_reader.get(url)
+    remote_url = Repo.remotes[0].config_reader.get(url)
     repo_name = os.path.splitext(os.path.basename(remote_url))[0]
 
     os.mkdir(f'/opt/{repo_name}')
