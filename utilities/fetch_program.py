b915cd3e710f0f780eed00878245378ac97b2118
Justin Merrell <merrelljustin@gmail.com>
2022-05-02 13:29:22 -0400

    Update fetch_program.py

--- utilities/fetch_program.py
+++ utilities/fetch_program.py
@@ -1,8 +1,14 @@
 ''' Fetches a program from proved URL and run the installer. '''
 
+import os
+
 from git import Repo
 
 
 def clone(url):
     ''' Clones the repository from the given URL. '''
-    Repo.clone_from(url, '/opt')
+    repo = Repo(url)
+    repo_name = repo.remotes.origin.url.split('.git')[0].split('/')[-1]
+
+    os.path.join('/opt/', repo_name)
+    Repo.clone_from(url, f'/opt/{repo_name}')
