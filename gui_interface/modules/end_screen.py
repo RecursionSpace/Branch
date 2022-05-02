4b48506291e0a04b3b34ab09d15423d8b3d812a3
Justin Merrell <merrelljustin@gmail.com>
2022-05-02 14:34:07 -0400

    Update end_screen.py

--- gui_interface/modules/end_screen.py
+++ gui_interface/modules/end_screen.py
@@ -20,11 +20,14 @@ class EndScreen(ttk.Frame):
         )
         label.pack(ipadx=20, ipady=20)
 
-        with open('/opt/Branch/branch.json', 'r+', encoding="utf-8") as json_file:
-            branch_settings = json.load(json_file)
+        try:
+            with open('/opt/Branch/branch.json', 'r+', encoding="utf-8") as json_file:
+                branch_settings = json.load(json_file)
 
-        program_label = ttk.Label(
-            self, text=f"Program: {branch_settings['program_name']}",
-            font=("Helvetica", 32)
-        )
-        program_label.pack(ipadx=20, ipady=20)
+            program_label = ttk.Label(
+                self, text=f"Program: {branch_settings['program_name']}",
+                font=("Helvetica", 32)
+            )
+            program_label.pack(ipadx=20, ipady=20)
+        except KeyError as err:
+            print(err)
