a3e2d08ac53f75c56dad9177092be50d53c71916
Justin Merrell <merrelljustin@gmail.com>
2022-05-02 17:13:38 -0400

    Update ssh_keys.py

--- utilities/ssh_keys.py
+++ utilities/ssh_keys.py
@@ -18,8 +18,9 @@ def new_deploy_key():
         sys.exit(1)
 
     # Create the key pair.
+
     subprocess.call(['ssh-keygen', '-t', 'ed25519', '-C',
-                    f'{os.environ["USER"]}', '-f', '~/.ssh/id_rsa', '-q', '-N', '""'])
+                    f'{os.environ["USER"]}', '-q', '-N', '""'])
     subprocess.call(['chmod', '600', '~/.ssh/id_rsa'])
     subprocess.call(['chmod', '644', '~/.ssh/id_rsa.pub'])
 
