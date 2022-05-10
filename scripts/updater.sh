66d6a2e68443f21bf130e159fd838fde2af26803
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 09:49:12 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -2,9 +2,9 @@
 
 # Perform updates of the branch program.
 
-branch_dir=$(jq '.branch.directory' /opt/Stem/stem.json)
-config_file=$(jq '.branch.config_file' /opt/Stem/stem.json)
+branch_dir=$(jq --raw-output '.branch.directory' /opt/Stem/stem.json)
+config_file=$(jq --raw-output '.branch.config_file' /opt/Stem/stem.json)
 
-current_version=$(jq '.current_version' "${branch_dir}${config_file}")
+current_version=$(jq --raw-output '.current_version' "${branch_dir}${config_file}")
 
 echo "Current version: $current_version"
