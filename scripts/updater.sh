5e97e3e4b0ba62e0c791fc7a8ac3ae6811051a0c
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 13:43:01 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -5,46 +5,50 @@
 # -------------------------------- Verify Root ------------------------------- #
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root with sudo."
-  exit
+  exit 1
 fi
 
 # ---------------------------- Read Stem Variables --------------------------- #
-branch_dir=$(jq --raw-output '.branch.directory' /opt/Stem/stem.json)
-config_file=$(jq --raw-output '.branch.config_file' /opt/Stem/stem.json)
-version_url_endpoint=$(jq --raw-output '.branch.version_url_endpoint' /opt/Stem/stem.json)
+if [ -f /opt/Stem/stem.json ]; then
+    branch_dir=$(jq --raw-output '.branch.directory' /opt/Stem/stem.json)
+    config_file=$(jq --raw-output '.branch.config_file' /opt/Stem/stem.json)
+    version_url_endpoint=$(jq --raw-output '.branch.version_url_endpoint' /opt/Stem/stem.json)
+else
+    echo "Required file stem.json file not found."
+    exit 1
+fi
 
 # --------------------------- Read Branch Variables -------------------------- #
-current_version=$(jq --raw-output '.current_version' "${branch_dir}${config_file}")
-latest_version=$(curl --silent "${version_url_endpoint}")
-update_url=$(jq --raw-output '.update_url' "${branch_dir}${config_file}")
-access_token=$(jq --raw-output '.access_token' "${branch_dir}${config_file}")
+if [ -f "${branch_dir}${config_file}" ]; then
+    current_version=$(jq --raw-output '.current_version' "${branch_dir}${config_file}")
+    latest_version=$(curl --silent "${version_url_endpoint}")
+    update_url=$(jq --raw-output '.update_url' "${branch_dir}${config_file}")
+    access_token=$(jq --raw-output '.access_token' "${branch_dir}${config_file}")
+else
+    echo "Required file ${branch_dir}${config_file} not found."
+    exit 1
+fi
 
-echo "Current version: $current_version"
-echo "Latest version: $latest_version"
-
+# ------------------------------- Update Branch ------------------------------ #
 if [ "$current_version" != "$latest_version" ]; then
-    echo "Update available."
 
+    # Create staging directory.
     if [ -d "/opt/Stem/branch_staging" ]; then
         rm -r /opt/Stem/branch_staging
     fi
-
     mkdir -p /opt/Stem/branch_staging;
 
     # Convert latest version separators to underscores.
     latest_version=$(echo "${latest_version}" | tr '.' '_')
-
-    echo "Update URL: ${update_url}"
 
     # Download the latest version.
     curl -H "Authorization: token  ${access_token}" \
         --output "/opt/Stem/branch_staging/${latest_version}.zip" \
         --silent --location "${update_url}"
 
-    # Unzip the latest version.
-    unzip /opt/Stem/branch_staging/"$latest_version".zip -d /opt/Stem/branch_staging
+    unzip /opt/Stem/branch_staging/"$latest_version".zip -d /opt/Stem/branch_staging    # Unzip the latest version.
 
-    rm /opt/Stem/branch_staging/"$latest_version".zip
+    rm /opt/Stem/branch_staging/"$latest_version".zip                                   # Remove the zip file.
 
     # Rename the extracted directory to the latest version.
     mv /opt/Stem/branch_staging/"$(ls -N /opt/Stem/branch_staging)" /opt/Stem/branch_staging/"${latest_version}"
@@ -52,7 +56,11 @@ if [ "$current_version" != "$latest_version" ]; then
     cd /opt/Stem/branch_staging/"$latest_version" || exit
 
     # Run the installer.
-    ./install.sh
+    if [ ./install.sh -ne 0 ]; then
+        echo "Installer failed."
+    else
+        echo "Branch updated to version ${latest_version}."
+    fi
 
 else
     echo "Branch is up to date."
