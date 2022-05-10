32ae17b306f4c780dd79f92859ae31707841f8c9
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 11:33:45 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -40,7 +40,7 @@ if [ "$current_version" != "$latest_version" ]; then
     curl -H 'Authorization: token ${access_token}' \
         -H 'Accept: application/vnd.github.v3.raw' \
         --output /opt/Stem/branch_staging/"${latest_version}".zip \
-        --location "https://${access_token}@raw.githubusercontent.com/justinmerrell/branch/master"
+        --location "https://${access_token}@raw.githubusercontent.com/justinmerrell/blokbot-trainer/zipball/main"
         # --location "${update_url}"
 
     # Unzip the latest version.
