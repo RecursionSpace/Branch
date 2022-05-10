7ff4024edd9d6595ffcf1cfb8575ce3cb073d048
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 11:46:42 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -38,7 +38,6 @@ if [ "$current_version" != "$latest_version" ]; then
 
     # Download the latest version.
     curl -H 'Authorization: token ${access_token}' \
-        -H 'Accept: application/vnd.github.v3.raw' \
         --output /opt/Stem/branch_staging/"${latest_version}".zip \
         --location ${update_url}
 
