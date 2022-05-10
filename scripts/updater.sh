9ed906806055536a66339bbc9ee141fa312cbae4
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 14:11:19 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -56,7 +56,7 @@ if [ "$current_version" != "$latest_version" ]; then
     cd /opt/Stem/branch_staging/"$latest_version_underscored" || exit
 
     # Run the installer.
-    if [ ./install.sh -ne 0 ]; then
+    if [ "$(./install.sh)" -ne 0 ]; then
         echo "Installer failed."
     else
         echo "Branch updated to version ${latest_version}."
