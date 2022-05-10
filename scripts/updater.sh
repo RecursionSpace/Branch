d4b533d3ed16e10c514deea7ec1faf8c517fb2d3
Justin Merrell <merrelljustin@gmail.com>
2022-05-10 09:46:42 -0400

    Update updater.sh

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -5,6 +5,6 @@
 branch_dir=$(jq '.branch.directory' /opt/Stem/stem.json)
 config_file=$(jq '.branch.config_file' /opt/Stem/stem.json)
 
-current_version=$(jq '.current_version' $branch_dir$config_file)
+current_version=$(jq '.current_version' "${branch_dir}${config_file}")
 
 echo "Current version: $current_version"
